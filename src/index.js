3a852be535b7cba226dd054ed22433b2b2361eaa
azenkurt <azenkurit@gmail.com>
2019-09-23 17:34:19 +0300

    init: task "Morse code decoder"

--- src/index.js
+++ src/index.js
@@ -38,7 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    while ( expr.length > 0 ) {
+        q = expr.substring(0, 10);
+        arr.push ( q );
+        expr = expr.substring ( 10 );
+    }
+    let text = '';
+    arr.forEach ( el => {
+        if ( el === '**********') { text += ' ' }	
+        else {
+            let code = '';
+            while ( el.length > 0) {
+                let q = el.substring(0, 2);
+                switch ( q ) {
+                    case '00' : { break;}
+                    case '10' : { code += '.'; break; }
+                    case '11' : { code += '-'; break; }
+                }
+                el = el.substring(2);
+            }
+            text += MORSE_TABLE[code];
+        }
+    });
+    return text;
 }
 
 module.exports = {
